26a222618d4c822c0ce4d33f2f2ab98d8b4a3c3f
theerasun_tangngamchit <theerasun@gmail.com>
2020-07-09 11:24:50 +0700

    add SpaController.java for prevent 404 when open direct url.

---
+++ backend/src/main/java/de/jonashackt/springbootvuejs/controller/SpaController.java
@@ -0,0 +1,22 @@
+package de.jonashackt.springbootvuejs.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller()
+@RequestMapping("/")
+public class SpaController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BackendController.class);
+
+    // Forwards all routes to FrontEnd except: '/', '/index.html', '/api', '/api/**'
+    // Required because of 'mode: history' usage in frontend routing, see README for further details
+    @RequestMapping(value = "{_:^(?!index\\.html|api).*$}")
+    public String redirectSpa() {
+        LOG.debug("URL entered directly into the Browser, so we need to redirect...");
+        
+        return "forward:/";
+    }
+}
